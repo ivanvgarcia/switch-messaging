ed8c366fd95bfcbec5c797e31e0a66189a7bf399
Ivan <ivanvlora@gmail.com>
2019-04-12 11:49:21 +0900

    styled and design the register component

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,8 +1,79 @@
 import React, { Component } from "react";
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 class Register extends Component {
+  state = {};
+  handleChange = () => {};
   render() {
-    return <div>Register</div>;
+    return (
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="red" textAlign="center">
+            <Icon name="puzzle piece" color="red" />
+            Register for Switch Messaging
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name="username"
+                icon="user"
+                label="Username"
+                iconPosition="left"
+                placeholder="Username"
+                onChange={this.handleChange}
+                type="text"
+              />
+              <Form.Input
+                fluid
+                name="email"
+                icon="mail"
+                label="Email"
+                iconPosition="left"
+                placeholder="Email Address"
+                onChange={this.handleChange}
+                type="text"
+              />
+              <Form.Input
+                fluid
+                name="password"
+                icon="lock"
+                label="Password"
+                iconPosition="left"
+                placeholder="Password"
+                onChange={this.handleChange}
+                type="password"
+              />
+              <Form.Input
+                fluid
+                name="passwordConfirmation"
+                icon="repeat"
+                label="Password Confirmation"
+                iconPosition="left"
+                placeholder="Password Confirmation"
+                onChange={this.handleChange}
+                type="password"
+              />
+              <Button color="red" fluid size="large">
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            Already a user? <Link to="/login">Login</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "../semantic/dist/semantic.min.css";
-
+import "semantic-ui-css/semantic.min.css";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 
@@ -12,8 +11,8 @@ const Root = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={App} />
-      <Router path="/login" component={Login} />
-      <Router path="/register" component={Register} />
+      <Route path="/register" component={Register} />
+      <Route path="/login" component={Login} />
     </Switch>
   </Router>
 );
