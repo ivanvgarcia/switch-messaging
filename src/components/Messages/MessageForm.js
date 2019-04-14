3bf50ef49bdf48d632d4ccdd33328e82711d09bc
Ivan <ivanvlora@gmail.com>
2019-04-14 23:18:49 +0900

    added a progress bar when uploading media

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -3,6 +3,7 @@ import uuidv4 from "uuidv4";
 import firebase from "../../firebase";
 import { Segment, Button, Input } from "semantic-ui-react";
 import FileModal from "./FileModal";
+import ProgressBar from "./ProgressBar";
 
 class MessageForm extends React.Component {
   state = {
@@ -78,7 +79,7 @@ class MessageForm extends React.Component {
     const filePath = `chat/public/${uuidv4()}.jpg`;
     this.setState(
       {
-        upload: "uploading...",
+        uploadState: "uploading",
         uploadTask: this.state.storageRef.child(filePath).put(file, metadata)
       },
       () => {
@@ -135,7 +136,14 @@ class MessageForm extends React.Component {
   };
 
   render() {
-    const { errors, message, loading, modal } = this.state;
+    const {
+      errors,
+      message,
+      loading,
+      modal,
+      uploadState,
+      percentUploaded
+    } = this.state;
 
     return (
       <Segment className="message__form">
@@ -170,12 +178,16 @@ class MessageForm extends React.Component {
             labelPosition="right"
             icon="cloud upload"
           />
-          <FileModal
-            modal={modal}
-            closeModal={this.closeModal}
-            uploadFile={this.uploadFile}
-          />
         </Button.Group>
+        <FileModal
+          modal={modal}
+          closeModal={this.closeModal}
+          uploadFile={this.uploadFile}
+        />
+        <ProgressBar
+          uploadState={uploadState}
+          percentUploaded={percentUploaded}
+        />
       </Segment>
     );
   }
